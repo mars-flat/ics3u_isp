f3215eadc3183d6b05669d09d7ae2058e65188e6
mars-flat <shanec2k05@gmail.com>
2022-01-07 15:01:42 -0500

    comment LeaderboardEntry.java

--- src/LeaderboardEntry.java
+++ src/LeaderboardEntry.java
@@ -1,6 +1,18 @@
+/*
+ * Shane Chen & Daniel Ye
+ * Ms. Basaraba
+ * January 7
+ * Class for leaderboard entries (for sorting and organization)
+ *
+ */
+
 public class LeaderboardEntry {
+
+    // data --> public for ease of access
     public final String entryName;
     public final int score;
+
+    // constructor
     public LeaderboardEntry(String eName, int eScore) {
         entryName = eName;
         score = eScore;
